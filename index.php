7786e04b9e74b7c0c279f8d6332c58eba4f141e3
weedzcokie <weedzcokie@live.se>
2012-11-15 13:32:26 +0100

    update

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php 
-$root_folder = './files';
-if (file_exists($root_folder))
-{
-	$folder = scandir($root_folder);
-	foreach($folder as $file) {
-		if ($file == '..' || $file == '.')
-			continue;
-		$file = basename($file); // basic sanitisation
-		echo $file;
-		if (is_file($root_folder.'/'.$file))
-		{
-			echo('<a href="'.$root_folder.'/'.$file.'">Download</a>');
-		}
-	}
-}
+require_once('./include/inc.listfiles.php');
 ?>
