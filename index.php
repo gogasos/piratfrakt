4f4e44a7e3e27305d64de675ce53e90794e3ef9a
weedzcokie <weedzcokie@live.se>
2012-11-15 13:46:20 +0100

    update

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php 
-require_once('./include/inc.listfiles.php');
+﻿<?php 
+require_once('./include/inc.core.php');
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="html/text; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="style/style.css" />
+		<title>Piratbåt</title>
+	</head>
+	<body>
+	
+	</body>
+</html>
