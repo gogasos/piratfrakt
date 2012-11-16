306c79ab7db9c30c173f8ed2c122b1f6b19af975
gogasos <Gus.nabro@gmail.com>
2012-11-16 15:00:33 +0100

    La till Båten bakom search formen.
    ska see på lite <div> tags senare.

--- index.php
+++ index.php
@@ -8,15 +8,21 @@
 		<title>Piratfrakt</title>
 	</head>
 	<body>
+	
 <form class="form-wrapper" action=""" method="post">
         <input type="text" name="find" id="search" placeholder="Sök efter filer..." required>
         <input type="submit" value="go" id="submit">
 </form>
+
 <?php 
 if (isset($_POST['find']))
 {
 	require_once('./include/inc.listfiles.php');
 }
 ?>
+
+
+
+
 	</body>
 </html>
